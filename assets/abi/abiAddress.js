f1dbb2394ca9dea045081f72c4f64571f63dcf4d
taurus-app <trashcoindev@gmail.com>
2025-06-30 18:57:12 +0800

    Add files via upload

--- assets/abi/abiAddress.js
+++ assets/abi/abiAddress.js
@@ -1,7 +1,7 @@
 // Contract addresses configuration
 // todo change contract address
 const CONTRACT_ADDRESSES = {
-    TAURUS: '0xead9f7ee095232193BC53A0DF84e33f0f20921c0'
+    TAURUS: '0x0877b9f51fd24a1D4f117EB910cF5b7180E1Dafd'
 };
 
 // Export for use in other files
